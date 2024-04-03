3f11bf3e5479c9b4cb148c0e840edbdc9ea94a7f
krimamadhikarmi <madhikrima20@gmail.com>
2024-04-03 17:05:34 +0545

    api get additional

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -2,15 +2,17 @@ class Api::V1::ReviewsController < ApplicationController
     def index
         movie = Movie.find_by(mid: params[:movie_id])
         if movie
-
             reviews = movie.reviews.map { |review| { user_id: review.user_id, description: review.description } }
-            if reviews
-            render json: { movie_id: movie.id,review_count: reviews.count, reviews: reviews }
-            else
-            render json: { error: 'Movie not found' }
-            end
+            render json: { movie_id: movie.mid,review_count: reviews.count, reviews: reviews }
         else
-            render json: { error: 'Movie not found' }
+            moviecreate= Tmdb::DetailService.execute(id: params[:movie_id])
+            # if moviecreate
+            #     Movie.create(mid: moviecreate['id'], title: moviecreate['title'], original_title: moviecreate['original_title'], overview: moviecreate['overview'],status: moviecreate['status'],release_date: moviecreate['release_date'],budget: moviecreate['budget'] ,poster: moviecreate['poster_path'], popularity: moviecreate['popularity'])
+            #     index()
+            # else
+            #     render json: {error: "Movie not found"},status:404
+            # end
         end
     end
 end
+
