d8caa48174e63ba981a012f923a7afdb945cd24f
krimamadhikarmi <madhikrima20@gmail.com>
2024-04-04 16:54:22 +0545

    modification

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -8,44 +8,43 @@ class Api::V1::ReviewsController < ApplicationController
         else
             moviecreate=fetch_movie(params[:movie_id])
             if moviecreate['id']
-                Movie.create(mid: moviecreate['id'], title: moviecreate['title'], original_title: moviecreate['original_title'], overview: moviecreate['overview'],status: moviecreate['status'],release_date: moviecreate['release_date'],budget: moviecreate['budget'] ,poster: moviecreate['poster_path'], popularity: moviecreate['popularity'])
+                create_movie_api(moviecreate)
                 index()
             elsif moviecreate['status_code']==6 || moviecreate['status_code']==34
-                render json: {error: "Movie not found"},status:404
+                movie_error()
             else
-                render json: {error: "Movie not found"},status:404
+                movie_error()
             end
         end
     end    
     
     def create
         movie = Movie.find_by(mid: params[:movie_id])
-
         if movie
-            reviews = Review.new(review_params.merge(user_id: User.first.id, movie_id: movie.id))
+            reviews = Review.create(review_params.merge(user_id: User.first.id, movie_id: movie.id))
             if reviews.save
                 reviews = movie.reviews.map { |review| { user_id: review.user_id, description: review.description } }
                 render json: { movie_id: movie.mid, reviews: reviews }
             else
-            render json: { error: "reviews not create" }
+                render json: { error: "Reviews not create" }
             end
         else
             moviecreate=fetch_movie(params[:movie_id])
             if moviecreate['id']
-                Movie.create(mid: moviecreate['id'], title: moviecreate['title'], original_title: moviecreate['original_title'], overview: moviecreate['overview'],status: moviecreate['status'],release_date: moviecreate['release_date'],budget: moviecreate['budget'] ,poster: moviecreate['poster_path'], popularity: moviecreate['popularity'])
+                create_movie_api(moviecreate)
                 movie = Movie.find_by(mid: params[:movie_id])
-                reviews = Review.new(review_params.merge(user_id: User.first.id, movie_id: movie.id))
+                reviews = Review.create(review_params.merge(user_id: User.first.id, movie_id: movie.id))
                 
                 if reviews.save
                     reviews = movie.reviews.map { |review| { user_id: review.user_id, description: review.description } }
                     render json: { movie_id: movie.mid, reviews: reviews }
                 else
-                    render json: { error: "reviews not create" }
+                    render json: { error: "Reviews not create" }
                 end
             elsif moviecreate['status_code']==6 || moviecreate['status_code']==34
-                render json: {error: "Movie not found"},status:404
+               movie_error()
             else
-                render json: {error: "Movie not found"},status:404
+                movie_error()
             end
         end
     end
@@ -56,10 +55,20 @@ class Api::V1::ReviewsController < ApplicationController
         movieresponse= Tmdb::DetailService.execute(id: movie_id)
     end
 
-
     def review_params
         params.require(:review).permit(:description)
     end
+
+    def movie_error
+        render json: {error: "Movie not found"},status:404
+    end
+
+    def create_movie_api(moviecreate)
+        Movie.create(mid: moviecreate['id'], title: moviecreate['title'], original_title: moviecreate['original_title'], overview: moviecreate['overview'],status: moviecreate['status'],
+        release_date: moviecreate['release_date'],budget: moviecreate['budget'] ,poster: moviecreate['poster_path'], 
+        popularity: moviecreate['popularity'])
+    end
+    
 end
 
 
